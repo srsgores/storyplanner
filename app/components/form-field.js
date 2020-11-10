01dd3cb0e2b34350293e3af39ae86eba99e0555a
Sean Goresht <seangores@gmail.com>
2020-11-09 21:45:02 -0700

    feat: add class names for saving/dirty states

--- app/components/form-field.js
+++ app/components/form-field.js
@@ -1,7 +1,7 @@
 import Component from "@glimmer/component";
 import {assert} from "@ember/debug";
 import {guidFor} from "@ember/object/internals";
-import {computed} from "@ember/object";
+import {action, computed} from "@ember/object";
 import {tracked} from "@glimmer/tracking";
 import {isEmpty} from "@ember/utils";
 import guessType from "ember-former/utils/guess-type";
@@ -11,7 +11,9 @@ export const CLASS_NAMES = {
 	FORM_CONTROL: "form-control",
 	FORM_FIELD: "form-field",
 	LABEL: "form-field-label",
-	FORM_FIELD_WITH_VALUE: "has-value"
+	FORM_FIELD_WITH_VALUE: "has-value",
+	SAVING: "saving",
+	UNSAVED: "unsaved"
 };
 
 export const ERROR_MESSAGES = {
@@ -26,6 +28,8 @@ export default class FormFieldComponent extends Component {
 	formControlClassName = CLASS_NAMES.FORM_CONTROL;
 	labelClassName = CLASS_NAMES.LABEL;
 	hasValueClassName = CLASS_NAMES.FORM_FIELD_WITH_VALUE;
+	savingClassName = CLASS_NAMES.SAVING;
+	unsavedClassName = CLASS_NAMES.UNSAVED;
 
 	@tracked hasFocus = false;
 
@@ -46,5 +50,11 @@ export default class FormFieldComponent extends Component {
 		super(...arguments);
 		assert(ERROR_MESSAGES.NO_MODEL_SUPPLIED_MESSAGE, this.args.model);
 		assert(ERROR_MESSAGES.NO_FIELD_NAME_SUPPLIED_MESSAGE, this.args.field);
+	}
+
+	@action autosave() {
+		if (this.args.autosave !== false) {
+			this.args.model.save();
+		}
 	}
 }
