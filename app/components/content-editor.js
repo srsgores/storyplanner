3aa9c95832488302b084be7b77a45ab94a90262a
Sean Goresht <seangores@gmail.com>
2020-11-17 00:22:34 -0700

    refactor: use content editor for rich editor

--- app/components/content-editor.js
+++ app/components/content-editor.js
@@ -1,4 +1,13 @@
-import Component from "@glimmer/component";
+import MobileDocEditorComponent from "ember-mobiledoc-editor/components/mobiledoc-editor/component";
+import {action} from "@ember/object";
 
-export default class ContentEditorComponent extends Component {
+export default class ContentEditorComponent extends MobileDocEditorComponent {
+	@action setupEditorShortcuts(editor) {
+		editor.onTextInput({
+			text: "> ",
+			run(editorInstance) {
+				editorInstance.toggleSection("blockquote");
+			}
+		});
+	}
 }
