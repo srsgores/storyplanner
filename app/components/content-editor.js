6a25932d6eef150cde8550c0eaefe7722aaeca55
Sean Goresht <seangores@gmail.com>
2020-11-22 01:42:21 -0700

    feat: allow adding dividers via --- shortcut key

--- app/components/content-editor.js
+++ app/components/content-editor.js
@@ -1,10 +1,12 @@
 import MobileDocEditorComponent from "ember-mobiledoc-editor/components/mobiledoc-editor/component";
 import {action} from "@ember/object";
-import {debounce} from "@ember/runloop";
+import {debounce, run} from "@ember/runloop";
 import {utils as ghostHelperUtils} from "@tryghost/helpers";
 import {tracked} from "@glimmer/tracking";
-
+import createComponentCard from "ember-mobiledoc-editor/utils/create-component-card";
 const {countWords} = ghostHelperUtils;
+
+export const SUPPORTED_CARDS = ["divider"];
 
 export default class ContentEditorComponent extends MobileDocEditorComponent {
 	@tracked autocompleteText;
@@ -12,24 +14,28 @@ export default class ContentEditorComponent extends MobileDocEditorComponent {
 	@tracked modelName = "character";
 	@tracked previousCursorPosition;
 
+	constructor() {
+		super(...arguments);
+		this.cards = SUPPORTED_CARDS.map((cardName) => createComponentCard(cardName));
+	}
 	get autocompletePrompt() {
 		return `Search for ${this.modelName}s`;
 	}
 
 	@action setupEditorShortcuts(editor) {
 		editor.onTextInput({
-			name: "content-editor",
+			name: "blockquote",
 			match: /^> /,
 			run(editor, matches) {
 				let {range} = editor;
-				let {head, head: {section}} = range;
-				let text = section.textUntil(head);
+				const {head, head: {section}} = range;
+				const text = section.textUntil(head);
 
 				// ensure cursor is at the end of the matched text so we don't convert text the users wants to start with `> ` and that we're not already on a blockquote section
 				if (text === matches[0] && section.tagName !== "blockquote") {
 					editor.run((postEditor) => {
 						range = range.extend(-(matches[0].length));
-						let position = postEditor.deleteRange(range);
+						const position = postEditor.deleteRange(range);
 						postEditor.setRange(position);
 
 						postEditor.toggleSection("blockquote");
@@ -48,6 +54,20 @@ export default class ContentEditorComponent extends MobileDocEditorComponent {
 				editor.replaceWithHeaderSection(editor, headingTag);
 			}
 		});
+
+		editor.onTextInput({
+			name: "md_hr",
+			match: /^---$/,
+			run: (postEditor) => {
+				const {range: {head, head: {section}}} = postEditor;
+				const payload = {};
+
+				// Skip if cursor is not at end of section
+				if (head.isTail() && !section.isListItem) {
+					this.replaceWithCardSection("divider", section.toRange(), payload);
+				}
+			}
+		});
 	}
 
 	@action setupCharacterMentions(editor) {
@@ -59,6 +79,30 @@ export default class ContentEditorComponent extends MobileDocEditorComponent {
 				this.showAutocomplete = true;
 				this.modelName = "character";
 				this.previousCursorPosiiton = editorInstance.position;
+			}
+		});
+	}
+
+	@action replaceWithCardSection(cardName, range, payload) {
+		const editor = this.editor;
+		const {head: {section}} = range;
+
+		editor.run((postEditor) => {
+			const {builder} = postEditor;
+			const card = builder.createCardSection(cardName, payload);
+			const nextSection = section.next;
+			const needsTrailingParagraph = !nextSection;
+
+			postEditor.replaceSection(section, card);
+
+			// add an empty paragraph after if necessary so writing can continue
+			if (needsTrailingParagraph) {
+				const newSection = postEditor.builder.createMarkupSection("p");
+				postEditor.insertSectionAtEnd(newSection);
+				postEditor.setRange(newSection.tailPosition());
+			}
+			else {
+				postEditor.setRange(nextSection.headPosition());
 			}
 		});
 	}
