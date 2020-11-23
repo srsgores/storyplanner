133c8e864c5287dbc68d1e4f99b2cdc62087b5c2
Sean Goresht <seangores@gmail.com>
2020-11-23 01:06:02 -0700

    feat: allow tagging other characters in character bio

--- app/routes/stories/story/characters/character.js
+++ app/routes/stories/story/characters/character.js
@@ -2,12 +2,6 @@ import Route from "@ember/routing/route";
 
 export default class StoriesStoryCharactersCharacterRoute extends Route {
 	model(params) {
-		const storyModel = this.modelFor("stories.story");
-		return this.store.findRecord("character", params.character_id).then((resolvedCharacter) => {
-			return {
-				character: resolvedCharacter,
-				story: storyModel
-			};
-		});
+		return this.store.findRecord("character", params.character_id);
 	}
 }
