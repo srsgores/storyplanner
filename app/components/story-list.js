8f30e143cd9934622148684c05e87279ebffdbe1
Sean Goresht <seangores@gmail.com>
2020-11-24 14:50:13 -0700

    fix: fix character mentions not showing up in story list

--- app/components/story-list.js
+++ app/components/story-list.js
@@ -1,8 +1,11 @@
 import Component from "@glimmer/component";
 import {action} from "@ember/object";
 import {inject as service} from "@ember/service";
+import {SUPPORTED_ATOMS, SUPPORTED_CARDS} from "./content-editor";
 
 export default class StoryListComponent extends Component {
+	cardNames = SUPPORTED_CARDS;
+	atomNames = SUPPORTED_ATOMS;
 	@service router;
 	@action async removeStory(story) {
 		await story.destroyRecord();
