9ae0f1a856ee60260f34237f995272ae81465dbc
Sean Goresht <seangores@gmail.com>
2020-12-02 00:35:51 -0700

    config: allow exporting data from localstorage

--- config/environment.js
+++ config/environment.js
@@ -6,6 +6,9 @@ module.exports = function(environment) {
 		environment,
 		rootURL: "/",
 		locationType: "auto",
+		"ember-local-storage": {
+			fileExport: true
+		},
 		EmberENV: {
 			FEATURES: {
 				// Here you can enable experimental features on an ember canary build
