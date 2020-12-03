6f235e7e39421a42e4a8d7ff10a0f74df7c151ab
Sean Goresht <seangores@gmail.com>
2020-12-02 22:40:59 -0700

    perf: only render date pickers when details toggled

--- app/components/node-editor.js
+++ app/components/node-editor.js
@@ -2,19 +2,30 @@ import Component from "@glimmer/component";
 import {action} from "@ember/object";
 import {guidFor} from "@ember/object/internals";
 import EventModel from "../models/event";
+import {tracked} from "@glimmer/tracking";
 
 export default class NodeEditorComponent extends Component {
+	@tracked eventTimingsToggled = false;
+
 	get hasSummary() {
 		return this.args.node.summary !== undefined;
 	}
+
 	get isEvent() {
 		return this.args.node instanceof EventModel;
 	}
+
 	get eventDuration() {
 		return this.args.node.endTime - this.args.node.startTime;
 	}
+
 	@action save() {
 		return this.args.node.save();
 	}
+
+	@action toggleEditEventTimings() {
+		this.eventTimingsToggled = !this.eventTimingsToggled;
+	}
+
 	elementId = guidFor(this);
 }
