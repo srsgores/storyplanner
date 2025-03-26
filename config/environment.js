4db9691529ee941db3783c08800b4d2268044f44
Sean Goresht <seangores@gmail.com>
2025-03-26 02:50:21 -0600

    chore: remove ember-local-storage configuration and simplify EXTEND_PROTOTYPES setting in environment.js

--- config/environment.js
+++ config/environment.js
@@ -6,9 +6,6 @@ module.exports = function (environment) {
 		environment,
 		rootURL: "/",
 		locationType: "auto",
-		"ember-local-storage": {
-			fileExport: true
-		},
 		"ember-simple-auth": {
 			routeAfterAuthentication: "authenticated.index",
 			routeIfAlreadyAuthenticated: "authenticated.index"
@@ -22,10 +19,7 @@ module.exports = function (environment) {
 				// Here you can enable experimental features on an ember canary build
 				// e.g. EMBER_NATIVE_DECORATOR_SUPPORT: true
 			},
-			EXTEND_PROTOTYPES: {
-				// Prevent Ember Data from overriding Date.parse.
-				Date: false
-			}
+			EXTEND_PROTOTYPES: false
 		},
 
 		APP: {
